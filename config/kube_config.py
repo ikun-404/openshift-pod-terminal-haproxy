1b486fb66d0459cb276f9967937400a623edc21b
ikun-404 <52433708+ikun-404@users.noreply.github.com>
2024-11-20 11:41:55 +0800

    Update kube_config.py

--- config/kube_config.py
+++ config/kube_config.py
@@ -1,7 +1,1 @@
-#! /usr/bin/python3
-# -*- coding:utf-8 -*-
-# @File:config.py
-# @Author:zhou jianhang
-# @Time:2024/09/03 14:18:09
-
 kubeconfig = "/root/openshift-client/openshift-requests-client/kubeconfig"
